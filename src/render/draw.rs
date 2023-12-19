5b58eba82a93f2f6bd1726bf6c2f59e1c1be3325
Leo Kling <leo.kling.info@gmail.com>
2023-12-19 14:42:52 +0100

    draw wall reflection factor

--- src/render/draw.rs
+++ src/render/draw.rs
@@ -124,9 +124,9 @@ pub fn draw_walls(
                             (origin.y + y as f32 * y_sign) as u32 + offset,
                         ),
                         Pixel {
-                            r: 255,
-                            g: 255,
-                            b: 255,
+                            r: (255. * wall.reflection_factor) as u8,
+                            g: (255. * wall.reflection_factor) as u8,
+                            b: (255. * wall.reflection_factor) as u8,
                             a: 255,
                         },
                     )
