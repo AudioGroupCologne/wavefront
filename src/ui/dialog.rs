59f43558fc1c5d643f95a1d91c132413b1b7933b
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-03-12 16:23:40 +0100

    reset on save

--- src/ui/dialog.rs
+++ src/ui/dialog.rs
@@ -2,9 +2,12 @@ use bevy::prelude::*;
 use bevy_file_dialog::DialogFileLoaded;
 use serde::Deserialize;
 
+use super::state::UiState;
 use crate::components::microphone::Microphone;
 use crate::components::source::Source;
 use crate::components::wall::{CircWall, RectWall};
+use crate::events::UpdateWalls;
+use crate::grid::grid::Grid;
 
 pub struct SaveFileContents;
 
@@ -19,10 +22,13 @@ pub struct SaveData {
 pub fn file_loaded(
     mut ev_loaded: EventReader<DialogFileLoaded<SaveFileContents>>,
     mut commands: Commands,
+    mut wall_update_ev: EventWriter<UpdateWalls>,
+    mut grid: ResMut<Grid>,
     sources: Query<(Entity, &Source)>,
     mics: Query<(Entity, &Microphone)>,
     rect_walls: Query<(Entity, &RectWall)>,
     circ_walls: Query<(Entity, &CircWall)>,
+    ui_state: Res<UiState>,
 ) {
     if let Some(data) = ev_loaded.read().next() {
         let save_data = serde_json::from_slice::<SaveData>(&data.contents).unwrap();
@@ -54,5 +60,8 @@ pub fn file_loaded(
         for circ_wall in save_data.circ_walls {
             commands.spawn(circ_wall);
         }
+
+        grid.reset_cells(ui_state.boundary_width);
+        wall_update_ev.send(UpdateWalls);
     }
 }
