f6fa2e5510cedb1a61966b7876df46ab1ef6346f
Leo Kling <leo.kling.info@gmail.com>
2024-04-05 14:03:49 +0200

    include icon in binary

--- src/main.rs
+++ src/main.rs
@@ -37,15 +37,11 @@ fn main() {
         .run();
 }
 
-fn set_window_icon(
-    // we have to use `NonSend` here
-    windows: NonSend<WinitWindows>,
-) {
-    // here we use the `image` crate to load our icon data from a png file
-    // this is not a very bevy-native solution, but it will do
+fn set_window_icon(windows: NonSend<WinitWindows>) {
     let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open("assets/icon.png")
-            .expect("Failed to open icon path")
+        let image = include_bytes!("../assets/icon.png");
+        let image = image::load_from_memory(image)
+            .expect("Failed to load icon from memory")
             .into_rgba8();
         let (width, height) = image.dimensions();
         let rgba = image.into_raw();
@@ -53,7 +49,6 @@ fn set_window_icon(
     };
     let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
 
-    // do it for all windows
     for window in windows.windows.values() {
         window.set_window_icon(Some(icon.clone()));
     }
