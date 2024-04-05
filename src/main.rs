58f7b050ad691d5b071bf1047069c357e1473757
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-04-05 15:51:48 +0200

    horrid icon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::prelude::*;
 use bevy::window::PresentMode;
+use bevy::winit::WinitWindows;
 use bevy_pixel_buffer::bevy_egui::EguiPlugin;
 use bevy_pixel_buffer::prelude::*;
 use wavefront::events::EventPlugin;
@@ -9,6 +10,7 @@ use wavefront::render::plugin::RenderPlugin;
 use wavefront::simulation::plugin::GridPlugin;
 use wavefront::ui::plugin::UiPlugin;
 use wavefront::undo::UndoPlugin;
+use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -31,5 +33,28 @@ fn main() {
             EventPlugin,
             UndoPlugin,
         ))
+        .add_systems(Startup, set_window_icon)
         .run();
+}
+
+fn set_window_icon(
+    // we have to use `NonSend` here
+    windows: NonSend<WinitWindows>,
+) {
+    // here we use the `image` crate to load our icon data from a png file
+    // this is not a very bevy-native solution, but it will do
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open("assets/icon.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+
+    // do it for all windows
+    for window in windows.windows.values() {
+        window.set_window_icon(Some(icon.clone()));
+    }
 }
