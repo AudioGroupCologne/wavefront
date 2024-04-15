36359923212c584eba1ea1b6c7f52fe92b38e687
Leo Kling <leo.kling.info@gmail.com>
2024-04-15 15:18:51 +0200

    keep selected place type

--- src/ui/draw.rs
+++ src/ui/draw.rs
@@ -1131,25 +1131,26 @@ pub fn draw_egui(
 
                 ui.set_enabled(!ui_state.render_abc_area);
 
-                match &mut ui_state.current_tool {
-                    ToolType::Place(t) => {
+                match ui_state.current_tool {
+                    ToolType::Place(_) => {
                         egui::ComboBox::from_label("Select Object to Place")
-                            .selected_text(format!("{}", t))
+                            .selected_text(format!("{}", ui_state.cur_place_type))
                             .show_ui(ui, |ui| {
                                 ui.style_mut().wrap = Some(false);
-                                ui.selectable_value(t, PlaceType::Source, "Source");
-                                ui.selectable_value(t, PlaceType::Mic, "Microphone");
-                                ui.selectable_value(t, PlaceType::RectWall, "Rectangle Wall");
-                                ui.selectable_value(t, PlaceType::CircWall, "Circular Wall");
+                                ui.selectable_value(&mut ui_state.cur_place_type, PlaceType::Source, "Source");
+                                ui.selectable_value(&mut ui_state.cur_place_type, PlaceType::Mic, "Microphone");
+                                ui.selectable_value(&mut ui_state.cur_place_type, PlaceType::RectWall, "Rectangle Wall");
+                                ui.selectable_value(&mut ui_state.cur_place_type, PlaceType::CircWall, "Circular Wall");
                             });
 
-                        if matches!(t, PlaceType::RectWall | PlaceType::CircWall) {
+                        if matches!(ui_state.cur_place_type, PlaceType::RectWall | PlaceType::CircWall) {
                             ui.add(
                                 egui::Slider::new(&mut ui_state.wall_reflection_factor, 0.0..=1.0)
                                     .text("Wall Reflection Factor"),
                             );
                             ui.checkbox(&mut ui_state.wall_is_hollow, "Hollow");
                         }
+                        ui_state.current_tool = ToolType::Place(ui_state.cur_place_type);   
                     }
                     _ => {
                         ui.add_space(10.);
@@ -1240,7 +1241,7 @@ pub fn draw_egui(
                 .clicked()
             {
                 // TODO: always resets to source when clicked
-                ui_state.current_tool = ToolType::Place(PlaceType::Source);
+                ui_state.current_tool = ToolType::Place(ui_state.cur_place_type);
             }
             ui.add_space(4.);
 
