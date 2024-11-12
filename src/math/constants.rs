260e6b2ae718edc44722689f61a96bbae0f1aa29
Leo Kling <leo.kling.info@gmail.com>
2024-11-12 15:56:13 +0100

    change propagation speed to compensate for tlm-error

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -4,8 +4,8 @@ pub const SIMULATION_WIDTH: u32 = 700;
 /// Amount of simulated pixels in the y direction
 pub const SIMULATION_HEIGHT: u32 = 700;
 
-/// Propagation speed of a sound wave in air (m/s)
-pub const PROPAGATION_SPEED: f32 = 343.2;
+/// Propagation speed of a sound wave in air (m/s) (* sqrt(2) to compensate for TLM-Error)
+pub const PROPAGATION_SPEED: f32 = 343.2 * 1.41421356237;
 
 /// Width of the boundary in pixels
 pub const INIT_BOUNDARY_WIDTH: u32 = 50;
