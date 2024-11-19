9b43ed81f4f86f1f425e66744e9872160e18dee6
Leo Kling <leo.kling.info@gmail.com>
2024-11-19 12:02:25 +0100

    use std constants

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -5,7 +5,7 @@ pub const SIMULATION_WIDTH: u32 = 700;
 pub const SIMULATION_HEIGHT: u32 = 700;
 
 /// Propagation speed of a sound wave in air (m/s) (* sqrt(2) to compensate for TLM-Error)
-pub const PROPAGATION_SPEED: f32 = 343.2 * 1.41421356237;
+pub const PROPAGATION_SPEED: f32 = 343.2 * std::f32::consts::SQRT_2;
 
 /// Width of the boundary in pixels
 pub const INIT_BOUNDARY_WIDTH: u32 = 50;
